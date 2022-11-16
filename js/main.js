8e40050dd1dedb6de5b3de304be39a451c30da83
fabianodsa <fabyanoduarte@hotmail.com>
2022-11-15 21:08:04 -0300

    botão enviar configurado para aparecer uma mensagem após clicado

--- js/main.js
+++ js/main.js
@@ -3,18 +3,27 @@
 const display = document.getElementById('display');
 const notaSelecionada = document.querySelectorAll('[id*=digito]');
 
+const botaoEnviarNota = document.getElementById('botao')
+
 const inserirNumero = (evento) => display.textContent = evento.target.textContent;
-//target demonstra o elemento que foi clicado. 
+//target monstra o elemento que foi clicado. Com o evento textContent, eu estou enviando para o disypay o texto que está dentro 
+// de cada uma dos números que foram clicados.
 
 notaSelecionada.forEach (notaSelecionada => notaSelecionada.addEventListener('click', inserirNumero));
 
-// function enviarNota () {
-//     const enviarNota = document.getElementById ('botao');
+// let armazenaNumero = notaSelecionada;
+
+// const guardarNumero = (evento) => {
+//     armazenaNumero = display.textContent;
 // }
 
 
-//- crie uma variável para armazenar o texto do botão clicado (por exemplo, valorDoBotao)
-//- coloque um evento de clique em cada botão
-//  - crie uma função para armazenar esse valor numa variável (salvarValorDoBotao()). Ela receberá um parâmetro (e), para representar o evento que a disparará.  Na implementação dela, vc atribui o texto do botão à variável criada para isso: valorDoBotão = e.target.innerText 
-// Ah, lembre de colocar a chamada dessa mesma função criada no evento de click de cada botão
-// <button onclick="salvarValorDoBotao()">
+// function armazenaNumero (armazenaNumero) {
+//     if notaSelecionada == 1 {
+//         return "Você selecionou 1";
+//     }
+// }
+
+botaoEnviarNota.addEventListener('click', () => {
+    alert("Você enviou a sua avaliação. Obrigado!")
+})
